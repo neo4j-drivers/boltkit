2e03a6b393ae723b6088fbca2565f8905f0bebef
lutovich <konstantin.lutovich@neotechnology.com>
2016-12-27 16:39:43 +0100

    v1.0.13

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except ImportError:
 packages = find_packages(exclude=("test", "test.*"))
 package_metadata = {
     "name": "boltkit",
-    "version": "1.0.12",
+    "version": "1.0.13",
     "description": "Toolkit for Neo4j 3.0+ driver authors",
     "author": "Neo Technology",
     "author_email": "drivers@neo4j.com",
