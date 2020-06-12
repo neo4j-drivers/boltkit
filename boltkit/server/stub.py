8da0c79df4c6b41805186d3d3641b44d0d65c351
martin bendsoe <martin.bendsoe@neotechnology.com>
2020-06-12 10:42:51 +0200

    updated the licence text

--- boltkit/server/stub.py
+++ boltkit/server/stub.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Copyright (c) 2002-2019 "Neo Technology,"
-# Network Engine for Objects in Lund AB [http://neotechnology.com]
+# Copyright (c) 2002-2020 "Neo4j,"
+# Neo4j Sweden AB [http://neo4j.com]
 #
 # This file is part of Neo4j.
 #
